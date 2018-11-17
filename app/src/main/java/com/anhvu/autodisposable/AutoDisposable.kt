c98932479bf89ba21d1a26f3da13c59bcff18c07
Anh Vu <tuananh.vu@ved.com.vn>
2018-11-17 13:30:11 +0700

    init

---
+++ app/src/main/java/com/anhvu/autodisposable/AutoDisposable.kt
@@ -0,0 +1,32 @@
+package com.anhvu.autodisposable
+
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+
+class AutoDisposable : LifecycleObserver {
+    lateinit var compositeDisposable: CompositeDisposable
+    fun bindTo(lifecycle: Lifecycle) {
+        lifecycle.addObserver(this)
+        compositeDisposable = CompositeDisposable()
+    }
+
+    fun add(disposable: Disposable) {
+        if (::compositeDisposable.isInitialized) {
+            compositeDisposable.add(disposable)
+        } else {
+            throw NotImplementedError("must bind AutoDisposable to a Lifecycle first")
+        }
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    fun onDestroy() {
+        compositeDisposable.dispose()
+    }
+}
+
+fun Disposable.addTo(autoDisposable: AutoDisposable) {
+    autoDisposable.add(this)
+}
---
+++ app/src/main/java/com/anhvu/autodisposable/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.anhvu.autodisposable
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.TimeUnit
+
+class MainActivity : AppCompatActivity() {
+
+    private val autoDisposable = AutoDisposable()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        autoDisposable.bindTo(this.lifecycle)
+
+        val hello = findViewById<TextView>(R.id.tvHello)
+        btnSubmit.setOnClickListener {
+            val disposable = Observable.just("Hello, this text emitted by observable")
+                .delay(3000, TimeUnit.MILLISECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                    {
+                        Log.i(TAG, "onNext: $it")
+                        hello.text = it
+                        hello.visibility = View.VISIBLE
+                    },
+                    { Log.e(TAG, "Error: ${it.message}") }
+                )
+            disposable.addTo(autoDisposable)
+            autoDisposable.add(disposable)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG, "onDestroy")
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+}
